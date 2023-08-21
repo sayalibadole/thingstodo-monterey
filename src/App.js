24e06c3f746eb79f62860a0681e129f78951e9fb
sayalibadole <badolesayali@gmail.com>
2023-08-20 22:07:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 import toursData from './data';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-//const url = 'https://course-api.com/react-tours-project'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -15,24 +12,9 @@ function App() {
     setTours(newTours)
   }
 
-  // const fetchTours = async () => {
-  //   setLoading(true)
-  //   try {
-  //     const response = await fetch(url)
-  //     const tours = await response.json()
-  //     setLoading(false)
-  //     setTours(tours)
-  //   } catch (error) {
-  //     setLoading(false)
-  //     console.log(error)
-  //   }
-  // }
-  // useEffect(() => {
-  //   fetchTours()
-  // }, [])
   useEffect(() => {
     setLoading(false);
-    setTours(toursData); // Use the imported data
+    setTours(toursData); 
   }, []);
 
   if (loading) {
